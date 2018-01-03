4097b9aee5ec7d8fd4316dba577736082757c5a2
Matt <wrestler.411@comcast.net>
2018-01-03 09:56:08 -0800

    setting the file path and using streamreader

---
+++ manhatten/manhatten/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using QuickType;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace manhatten
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //setting the path
+            string fileName = "data.json";
+            string path = Path.Combine(Environment.CurrentDirectory, @"manhatten\", fileName);
+            string jsonStuffs;
+
+            //using streamreader to read json file
+            using (StreamReader sr = File.OpenText(path))
+            {
+                jsonStuffs = File.ReadAllText(path);
+                Console.WriteLine(jsonStuffs);
+                Console.ReadLine();
+                Console.Clear();
+            }
+        }
+
+
+    }
+}
